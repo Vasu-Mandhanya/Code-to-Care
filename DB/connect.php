ae64057a9d26520c6e667505238b57d8f6fb2f5a
Vasu-Mandhanya <55338336+Vasu-Mandhanya@users.noreply.github.com>
2020-11-20 19:30:47 +0530

    Initial

---
+++ DB/connect.php
@@ -0,0 +1,17 @@
+ <?php
+$servernamedb = "localhost";
+$usernamedb = "root";
+$passworddb = "mysql";
+$dbname="id15223135_covital";
+
+// Create connection
+$conn = new mysqli($servernamedb, $usernamedb, $passworddb,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+else{
+    //echo "successful";
+}
+?>
